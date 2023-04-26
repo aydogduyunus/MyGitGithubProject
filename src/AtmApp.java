382948035f60f61ad8ad7b8a548d0656b689df7d
Yunus AYDOGDU <aydogduyunuss@Yunus-MacBook-Air.local>
2023-04-26 12:22:16 +0300

    v6

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -3,17 +3,17 @@ public class AtmApp {
     public static void main(String[] args) {
 
         System.out.println("Bu bir ATM projesidir.");
-
-        //version
-        System.out.println("Java");
-        //version 2
+        //version1
+        System.out.println("Java is easy!");
+        //version2
         System.out.println("Learn Java earn money!!");
-        //version 3
-        System.out.println("Javayı öğrenmek çok kolay");
-        //version 4
-        System.out.println("Git öğrenmek ne kadar da zormuş");
-        //version 5
+        //version3
+        System.out.println("Javayı öğrenmek çok kolay:)");
+        //version4
 
+        //version5
+        System.out.println("Git öğrenmek ne kadar da keyifli ve zevkli;)");
+        //version6
 
 
 
